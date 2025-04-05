695a278ae087cedcd937896c960e1204e01aac00
shibayancodes <shibayanbhattacharya4@gmail.com>
2025-04-06 03:37:59 +0530

    added the thing in the repo

---
+++ new.js
@@ -0,0 +1,9 @@
+const oogabooga = {
+    name : "Shibayan",
+    Lastname : "Bhattacharjee",
+    rollno : "16500222047",
+    class : "123342423",
+    troll : "ADKSDlhhh"
+};
+
+console.log(oogabooga);
